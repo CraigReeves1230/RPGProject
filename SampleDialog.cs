3fcfb66e375e51aae9e1f022c656e031ce7ae04b
cdreeves <reevescd1@gmail.com>
2022-12-18 17:58:46 -0500

    rpg-7

--- SampleDialog.cs
+++ SampleDialog.cs
@@ -16,27 +16,30 @@ public class SampleDialog : EventSequence
     
     public override void run()
     {
-        walkEast(GameManager.instance.partyLead(), 4f);
+        var blondie = GameManager.instance.partyLead();
+        var baldman = GameManager.instance.party[1];
+        
+        walkEast(blondie, 4f);
         delay(1f);
         msg(":name Blondie :face 0", "This is a test. Yada, yada yada.");
         wait();
         msg("For I am Blondie, the princess of all Saiyans!");
         wait();
         msgCls();
-        runSouth(GameManager.instance.partyLead(), 4f);
+        runSouth(blondie, 4f);
         delay(1f);
         msg(":name Baldman :face 1", "This is yet another test. Yada, yada yada.");
         wait();
         msgCls();
         stopAllFollowing();
-        walkWest(GameManager.instance.party[1], 4f);
-        faceWest(GameManager.instance.partyLead());
+        walkWest(baldman, 4f);
+        faceWest(blondie);
         delay(.5f);
         msg(":name Baldman :face 1", "I don't know, Blondie. I get the impression we're in for a pretty wild ride.");
         wait();
         msg("I'd be on my P's and Q's...");
         wait();
-        walkWest(GameManager.instance.partyLead(), 2f);
+        walkWest(blondie, 2f);
         msg(":name Blondie :face 0", "You worry too much, Baldman. I'm really happy you're here.");
         wait();
         msgCls();
---
+++ SampleEgress.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleEgress : EventSequence
+{
+    public override void run()
+    {
+        
+    }
+}
