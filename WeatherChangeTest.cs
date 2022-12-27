dc66addd31c7a7da77e4300285c41d2954a96bd5
cdreeves <reevescd1@gmail.com>
2022-12-27 01:10:54 -0500

    RPG-6 lots of work done today

--- WeatherChangeTest.cs
+++ WeatherChangeTest.cs
@@ -8,12 +8,15 @@ public class WeatherChangeTest : EventCutscene
     protected override void doCutscene()
     {
         var r = GameManager.instance.partyLead();
+        var s = GameManager.instance.party[1];
         
-        msg(":name Reinhardt", "OK, here goes nothing...");
+        turnToFace(s, r.gameObject);
+        msg(":name Blondie :face 0", "OK, here goes nothing...");
         wait();
         msgCls();
+        setRain(false, true);
         delay(1f);
-        setSnow(true, false, 0, 1f);
+        setSnow(true, true, 0.01f, 1f);
         setFog(true, true, 0.01f, 0.005f);
         delay(4f);
         faceEast(r);
@@ -22,7 +25,9 @@ public class WeatherChangeTest : EventCutscene
         delay(2f);
         faceSouth(r);
         delay(2f);
-        msg(":name Reinhardt", "Well, well...looks like the snow came just in time.");
+        msg(":name Baldman :face 1", "Well, well...looks like the snow came just in time.");
+        wait();
+        msg(":name Blondie :face 0", "Aren't we so lucky, haha!");
         wait();
         msgCls();
     }
